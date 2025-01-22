e24a8c597b9a43d22a7987bec7d47aa5aa6769f7
manuteteo69 <manuel.abalo.rietz@rai.usc.es>
2025-01-22 10:21:46 +0100

    clase Servidor

--- src/chat_servidor/servidor/Servidor.java
+++ src/chat_servidor/servidor/Servidor.java
@@ -1,5 +1,49 @@
 package chat_servidor.servidor;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+import javax.swing.JOptionPane;
 
-public class Servidor {
-    
+public class Servidor extends Thread {    
+    private ServerSocket serverSocket;
+    LinkedList<HiloCliente> clientes;
+    private final VentanaS ventana;
+    private final String puerto;
+    static int correlativo;
+
+    public Servidor(String puerto, VentanaS ventana) {
+        correlativo = 0;
+        this.puerto = puerto;
+        this.ventana = ventana;
+        clientes = new LinkedList<>();
+        this.start();
+    }
+
+    public void run() {
+        try {
+            serverSocket = new ServerSocket(Integer.valueOf(puerto));
+            ventana.addServidorIniciado();
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("Nueva conexión entrante: " + socket);
+                HiloCliente h = new HiloCliente(socket, this);               
+                h.start();
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(ventana, "El servidor no se ha podido iniciar,\n"
+                                                 + "puede que haya ingresado un puerto incorrecto.\n"
+                                                 + "Esta aplicación se cerrará.");
+            System.exit(0);
+        }                
+    }        
+
+    LinkedList<String> getUsuariosConectados() {
+        LinkedList<String> usuariosConectados = new LinkedList<>();
+        clientes.forEach(c -> usuariosConectados.add(c.getIdentificador()));
+        return usuariosConectados;
+    }
+
+    void agregarLog(String texto) {
+        ventana.agregarLog(texto);
+    }
 }
