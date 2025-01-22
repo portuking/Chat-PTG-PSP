444e7895bc4cfdf6b8a2acf9f0bb50c15a329b0e
portuking <dopazopablo2004@gmail.com>
2025-01-22 09:51:09 +0100

    Parte do Cliente

--- src/chat_servidor/cliente/Cliente.java
+++ src/chat_servidor/cliente/Cliente.java
@@ -1,5 +1,130 @@
 package chat_servidor.cliente;
 
-public class Cliente {
-    
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.LinkedList;
+import javax.swing.JOptionPane;
+
+public class Cliente extends Thread {
+    private Socket socket;
+    private ObjectOutputStream objectOutputStream;
+    private ObjectInputStream objectInputStream;
+    private final VentanaC ventana;
+    private String identificador;
+    private boolean escuchando;
+    private final String host;
+    private final int puerto;
+
+    Cliente(VentanaC ventana, String host, Integer puerto, String nombre) {
+        this.ventana = ventana;
+        this.host = host;
+        this.puerto = puerto;
+        this.identificador = nombre;
+        escuchando = true;
+        this.start();
+    }
+
+    public void run() {
+        try {
+            socket = new Socket(host, puerto);
+            objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            objectInputStream = new ObjectInputStream(socket.getInputStream());
+            System.out.println("Conexion exitosa!");
+            this.enviarSolicitudConexion(identificador);
+            this.escuchar();
+        } catch (UnknownHostException ex) {
+            JOptionPane.showMessageDialog(ventana, "Conexión rehusada: servidor desconocido.");
+            System.exit(0);
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(ventana, "Error de Entrada/Salida.");
+            System.exit(0);
+        }
+    }
+
+    public void desconectar() {
+        try {
+            objectOutputStream.close();
+            objectInputStream.close();
+            socket.close();
+            escuchando = false;
+        } catch (Exception e) {
+            System.err.println("Error al cerrar los elementos de comunicación.");
+        }
+    }
+
+    public void enviarMensaje(String cliente_receptor, String mensaje) {
+        LinkedList<String> lista = new LinkedList<>();
+        lista.add("MENSAJE");
+        lista.add(identificador);
+        lista.add(cliente_receptor);
+        lista.add(mensaje);
+        try {
+            objectOutputStream.writeObject(lista);
+        } catch (IOException ex) {
+            System.out.println("Error al enviar mensaje al servidor.");
+        }
+    }
+
+    public void escuchar() {
+        try {
+            while (escuchando) {
+                Object aux = objectInputStream.readObject();
+                if (aux instanceof LinkedList) {
+                    ejecutar((LinkedList<String>) aux);
+                }
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(ventana, "Se perdió la comunicación con el servidor.");
+            System.exit(0);
+        }
+    }
+
+    public void ejecutar(LinkedList<String> lista) {
+        String tipo = lista.get(0);
+        switch (tipo) {
+            case "CONEXION_ACEPTADA":
+                identificador = lista.get(1);
+                ventana.sesionIniciada(identificador);
+                for (int i = 2; i < lista.size(); i++) {
+                    ventana.addContacto(lista.get(i));
+                }
+                break;
+            case "NUEVO_USUARIO_CONECTADO":
+                ventana.addContacto(lista.get(1));
+                break;
+            case "USUARIO_DESCONECTADO":
+                ventana.eliminarContacto(lista.get(1));
+                break;
+            case "MENSAJE":
+                ventana.addMensaje(lista.get(1), lista.get(3));
+                break;
+        }
+    }
+
+    private void enviarSolicitudConexion(String identificador) {
+        LinkedList<String> lista = new LinkedList<>();
+        lista.add("SOLICITUD_CONEXION");
+        lista.add(identificador);
+        try {
+            objectOutputStream.writeObject(lista);
+        } catch (IOException ex) {
+            System.out.println("Error al enviar mensaje de conexión.");
+        }
+    }
+
+    void confirmarDesconexion() {
+        LinkedList<String> lista = new LinkedList<>();
+        lista.add("SOLICITUD_DESCONEXION");
+        lista.add(identificador);
+        try {
+            objectOutputStream.writeObject(lista);
+        } catch (IOException ex) {
+            System.out.println("Error al enviar mensaje de desconexión.");
+        }
+    }
+
+    String getIdentificador() {
+        return identificador;
+    }
 }
